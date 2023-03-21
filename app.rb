b178acf68c51bb3e5c548ded119dd30bd568ab69
Ryan Lai <ryanlai12@gmail.com>
2023-03-21 15:30:28 +0000

    Added screenshots

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@ require './lib/bank_account'
 require './lib/bank_statement_printer'
 
 bank_account = BankAccount.new
-
+bank_account.deposit(50000)
+bank_account.withdraw(30000)
 bank_account.deposit(100)
 bank_account.deposit(100)
 bank_account.deposit(1000)
 bank_account.withdraw(1200)
-BankStatementPrinter.printStatement(bank_account)
+BankStatementPrinter.print_statement(bank_account)
