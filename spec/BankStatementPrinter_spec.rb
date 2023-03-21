d91ebe8f26eac0c4f3012324c36e9166faaa55a6
Ryan Lai <ryanlai12@gmail.com>
2023-03-21 10:29:29 +0000

    Added test for BankStatementPrinter class

---
+++ spec/BankStatementPrinter_spec.rb
@@ -0,0 +1,19 @@
+require 'BankStatementPrinter'
+
+RSpec.describe BankStatementPrinter do
+
+    before(:each) do
+      @bank_account = BankAccount.new 
+      @bank_account.deposit(1000)
+      @bank_account.withdraw(500)
+    end
+
+    it "should print the statement, correctly formatted, in reverse chronological order" do
+      today = Time.now.strftime("%d/%m/%Y")
+      expect { BankStatementPrinter.printStatement(@bank_account) }.to output(
+        "date || credit || debit || balance\n" +
+        "#{today} ||  || 500.00 || 500.00\n" +
+        "#{today} || 1000.00 ||  || 1000.00\n"
+      ).to_stdout
+    end    
+end
